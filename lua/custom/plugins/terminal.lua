2d0b3cc39c879577712591866991a844f21b6e2c
Ekaum Singh <e4singh@uwaterloo.ca>
2026-08-20 18:33:46 -0400

    fix size

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,4 +1,4 @@
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap('n', '<leader>t', ':vsplit | terminal<CR>', opts)
+vim.keymap.set('n', '<leader>t', ':vsplit | vertical resize 40 | terminal<CR>', { desc = 'Open small terminal split' })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
